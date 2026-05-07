70246513c7d96de64745e8d9b9da7df3710a6ac7
董昊文 <dhw21@mails.tsinghua.edu.cn>
2026-05-07 10:15:20 +0800

    Refine

--- tidb-deploy.sh
+++ tidb-deploy.sh
@@ -2,4 +2,4 @@ source ./tidb.env
 
 tiup cluster check ${TOPOLOGY_FILE}
 tiup cluster check ${TOPOLOGY_FILE} --apply
-tiup cluster deploy ${CLUSTER_NAME} ${TOPOLOGY_FILE}
+tiup cluster deploy ${CLUSTER_NAME} ${CLUSTER_VERSION} ${TOPOLOGY_FILE}
