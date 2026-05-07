6897a08740f247869b1792d474197bff27f7de8a
董昊文 <dhw21@mails.tsinghua.edu.cn>
2026-05-07 15:19:26 +0800

    Fix something.

--- tidb-deploy.sh
+++ tidb-deploy.sh
@@ -1,5 +1,7 @@
 source ./tidb.env
 
-tiup cluster check ${TOPOLOGY_FILE}
-tiup cluster check ${TOPOLOGY_FILE} --apply
-tiup cluster deploy ${CLUSTER_NAME} ${CLUSTER_VERSION} ${TOPOLOGY_FILE}
+IDENTITY_FLAG="-i $KEY_SAVE_PATH"
+
+tiup cluster check ${TOPOLOGY_FILE} ${IDENTITY_FLAG}
+tiup cluster check ${TOPOLOGY_FILE} ${IDENTITY_FLAG} --apply
+tiup cluster deploy ${CLUSTER_NAME} ${CLUSTER_VERSION} ${TOPOLOGY_FILE} ${IDENTITY_FLAG}  
