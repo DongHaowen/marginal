41b63540c13d5299087f1c0494f97a4d450b2a5f
董昊文 <dhw21@mails.tsinghua.edu.cn>
2026-05-14 06:17:51 +0800

    Fix a path error.

--- sync.sh
+++ sync.sh
@@ -4,8 +4,7 @@
 source ./tidb.env
 
 # 添加到路径
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # git同步
 git pull
