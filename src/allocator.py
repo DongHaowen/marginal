29b58222f4270e76cb62fe3c938a38058303534e
董昊文 <dhw21@mails.tsinghua.edu.cn>
2026-05-28 14:18:08 +0800

    Refine.

--- src/allocator.py
+++ src/allocator.py
@@ -35,5 +35,36 @@ class Allocator:
     @abstractmethod
     def allocate(self) -> dict[str, list[str]]:
         pass
+
+
+class SimpleAllocator(Allocator):
+    def allocate(self) -> dict[str, list[str]]:
+        # 简单的分配算法：将租户平均分配到服务器上
+        allocation = {server_id: [] for server_id in self.server_map.keys()}
+        tenant_ids = list(self.tenant_map.keys())
+        num_servers = len(self.server_map)
+        
+        for i, tenant_id in enumerate(tenant_ids):
+            server_id = list(self.server_map.keys())[i % num_servers]
+            allocation[server_id].append(tenant_id)
+        
+        return allocation
+
+class SkewAllocator(Allocator):
+    def allocate(self) -> dict[str, list[str]]:
+        # 负载不均衡的分配算法：将所有租户分配到第一个服务器上
+        allocation = {server_id: [] for server_id in self.server_map.keys()}
+        tenant_ids = list(self.tenant_map.keys())
+        
+        if tenant_ids:
+            first_server_id = list(self.server_map.keys())[0]
+            allocation[first_server_id] = tenant_ids
+        
+        return allocation
+
+
     
+class MyAllocator(Allocator):
+    def allocate(self) -> dict[str, list[str]]:
+        pass
     
--- tidb/haproxy.sh
+++ tidb/haproxy.sh
@@ -4,7 +4,7 @@
 source ./tidb.env
 
 # 运行src/cluster.py进行初始化
-"$PYTHON_CMD" "$SRC_DIR/cluster.py"
+"$PYTHON_CMD" "$SRC_DIR/haproxy.py"
 
 # 重载haproxy服务
 sudo cp haproxy.cfg /etc/haproxy/haproxy.cfg
